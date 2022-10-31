13d8193be561724b10dc5916843b2531985bf576
Gustavo Henrique <Gustavo326silva@gmail.com>
2022-10-31 16:26:24 -0300

    Trabalhando com banco de dados

---
+++ src/arquivos/Arquivos.java
@@ -0,0 +1,27 @@
+package arquivos;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class Arquivos {
+
+    
+    public static void main(String[] args) {
+        
+        //Primeira barra do lado esquerdo anula o caractere seguinte
+        //String teste = "Olá \"Pedro\" ";
+        
+        //Passo 1 - Quem é o arquivo,Onde ele está
+        //Endereço
+        String arquivo = "C:\\Users\\22282186\\JavaBancoDeDados\\teste.txt";
+        
+        //Passo 2 - Criar um objeto Path "Caminho"
+        Path path = Paths.get(arquivo);
+        
+        //Passo 3 - Criar um escritor
+        Buffered
+        
+    }
+    
+}
